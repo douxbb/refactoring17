7f2915d70f43a258439501b789ae4c45e582c129
HeavyPunk <kirillkuchma8@gmail.com>
2021-11-09 23:50:09 +0500

    Часть 3

--- filter.py
+++ filter.py
@@ -11,25 +11,13 @@ def get_mosaic_image(image, mosaic_size=10, gradation=4):
     while i < image_length:
         j = 0
         while j < image_height:
-            colors_sum = get_matrix_elem_sum(image_tensor, mosaic_size, i, j)
+            sector = image_tensor[i: i + mosaic_size, j: j + mosaic_size]
+            colors_sum = np.sum(sector)
             average_color = int(colors_sum // (mosaic_size ** 2))
             set_color(int(average_color // threshold) * threshold / 3, image_tensor, mosaic_size, i, j)
             j += mosaic_size
         i += mosaic_size
     return Image.fromarray(np.uint8(image_tensor))
-
-
-def get_matrix_elem_sum(m, mosaic_size, i, j):
-    color_sum = 0
-    for sector_i in range(i, i + mosaic_size):
-        for sector_j in range(j, j + mosaic_size):
-            n1 = m[sector_i][sector_j][0]
-            n2 = m[sector_i][sector_j][1]
-            n3 = m[sector_i][sector_j][2]
-            M = n1 + n2 + n3
-            color_sum += M
-    return color_sum
-
 
 def set_color(new_color, tensor, mosaic_size, i, j):
     for sector_i in range(i, i + mosaic_size):

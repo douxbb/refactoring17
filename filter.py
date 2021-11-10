586df423eedc2ae062b854c458faa1264f1e2a3b
1shaman1 <79924068+1shaman1@users.noreply.github.com>
2021-11-10 21:51:31 +0500

    Add files via upload

--- filter.py
+++ filter.py
@@ -1,32 +1,48 @@
 from PIL import Image
 import numpy as np
+
+def scale_middle_brightness(arr, i, j, pixel_size, width, height):
+    middle_brightness = 0
+    for n in range(i, i + pixel_size):
+        if n >= width:
+            break
+        for n1 in range(j, j + pixel_size):
+            if n1 >= height:
+                break
+            r = int(arr[n][n1][0])
+            g = int(arr[n][n1][1])
+            b = int(arr[n][n1][2])
+            brightness = int(r + g + b) // 3
+            middle_brightness += brightness
+    return int(middle_brightness // (pixel_size ** 2))
+
+def refactor_pixels(arr, i, j, middle_brightness, step, pixel_size, width, height):
+    for n in range(i, i + pixel_size):
+        if n >= width:
+            break
+        for n1 in range(j, j + pixel_size):
+            if n1 >= height:
+                break
+            arr[n][n1][0] = int(middle_brightness // step) * step
+            arr[n][n1][1] = int(middle_brightness // step) * step
+            arr[n][n1][2] = int(middle_brightness // step) * step
+
+def pixelation(arr, step, pixel_size):
+    width = len(arr)
+    height = len(arr[1])
+    i = 0
+    while i < width:
+        j = 0
+        while j < height:
+            middle_brightness = scale_middle_brightness(arr, i, j, pixel_size, width, height)
+            refactor_pixels(arr, i, j, middle_brightness, step, pixel_size, width, height)
+            j = j + pixel_size
+        i = i + pixel_size
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
-width = len(arr)
-height = len(arr[1])
-i = 0
-while i < width:
-    j = 0
-    while j < height:
-        s = 0
-        for n in range(i, i + 10):
-            if n >= width:
-                break
-            for n1 in range(j, j + 10):
-                if n1 >= height:
-                    break
-                r = int(arr[n][n1][0])
-                g = int(arr[n][n1][1])
-                b = int(arr[n][n1][2])
-                M = int(r + g + b) // 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+
+pixelation(arr, 50, 12)
+
 res = Image.fromarray(arr)
 res.save('res.jpg')

5a8df8ec0d295351ba9180b19e2077f8ecdfbb79
1shaman1 <79924068+1shaman1@users.noreply.github.com>
2021-11-13 18:43:52 +0500

    Add files via upload

--- filter.py
+++ filter.py
@@ -1,48 +1,24 @@
 from PIL import Image
 import numpy as np
 
-def scale_middle_brightness(arr, i, j, pixel_size, width, height):
-    middle_brightness = 0
-    for n in range(i, i + pixel_size):
-        if n >= width:
-            break
-        for n1 in range(j, j + pixel_size):
-            if n1 >= height:
-                break
-            r = int(arr[n][n1][0])
-            g = int(arr[n][n1][1])
-            b = int(arr[n][n1][2])
-            brightness = int(r + g + b) // 3
-            middle_brightness += brightness
-    return int(middle_brightness // (pixel_size ** 2))
 
-def refactor_pixels(arr, i, j, middle_brightness, step, pixel_size, width, height):
-    for n in range(i, i + pixel_size):
-        if n >= width:
-            break
-        for n1 in range(j, j + pixel_size):
-            if n1 >= height:
-                break
-            arr[n][n1][0] = int(middle_brightness // step) * step
-            arr[n][n1][1] = int(middle_brightness // step) * step
-            arr[n][n1][2] = int(middle_brightness // step) * step
+def refactor_pixels(pixels, i, j, step, pixel_size):
+    middle_brightness = int(int(np.sum(pixels[i:i + pixel_size, j:j + pixel_size])) // 3 // (pixel_size ** 2))
+    pixels[i: i + pixel_size, j: j + pixel_size] = int(middle_brightness // step) * step
 
-def pixelation(arr, step, pixel_size):
-    width = len(arr)
-    height = len(arr[1])
-    i = 0
-    while i < width:
-        j = 0
-        while j < height:
-            middle_brightness = scale_middle_brightness(arr, i, j, pixel_size, width, height)
-            refactor_pixels(arr, i, j, middle_brightness, step, pixel_size, width, height)
-            j = j + pixel_size
-        i = i + pixel_size
+def pixelation(pixels, step, pixel_size):
+    width = len(pixels)
+    height = len(pixels[1])
+    pixels = pixels[:width // pixel_size * pixel_size, :height // pixel_size * pixel_size]
+    width = len(pixels)
+    height = len(pixels[1])
+    for i in range(0, width - pixel_size + 1, pixel_size):
+        for j in range(0, height - pixel_size + 1, pixel_size):
+            refactor_pixels(pixels, i, j, step, pixel_size)
+    return pixels
 
 img = Image.open("img2.jpg")
-arr = np.array(img)
-
-pixelation(arr, 50, 12)
-
-res = Image.fromarray(arr)
+pixels = np.array(img)
+res = Image.fromarray(pixelation(pixels, 50, 20))
 res.save('res.jpg')
+

a9283d38225918a0e6c8c1399a5e99968e2d7d98
Lilya <73344383+KingsWoman@users.noreply.github.com>
2021-11-14 18:13:40 +0300

    Step_4

--- filter.py
+++ filter.py
@@ -1,25 +1,35 @@
 import numpy as np
 from PIL import Image
-
-
 class GreyImage:
     def __init__(self, pix_array, pix_size=10, gradation=5):
         self.image = np.array(pix_array)
-        self.size = pix_size        
+        self.size = pix_size
         self.grad = 255 // gradation
         self.width = len(self.image)
         self.height = len(self.image[0])
-
-    def get_grey_image(self):        
+    def get_grey_image(self):
         for x in range(0, self.width, self.size):
-            for y in range(0, self.height, self.size):                
+            for y in range(0, self.height, self.size):
                     self.image[x:x + self.size, y:y + self.size] = self.get_middle_color(x, y)
-        return Image.fromarray(self.image)    
+        return Image.fromarray(self.image)
     def get_middle_color(self, x, y):
         return int(self.image[x:x + self.size, y:y + self.size].sum() / 3 // self.size ** 2 // self.grad * self.grad)
-
-
-
-orig_img = Image.open("img2.jpg")
-arr = GreyImage(orig_img, pix_size=int(input()), gradation=int(input())).get_grey_image()
-arr.save('res.jpg')
+flag = 'no'
+while flag == 'no':
+    print('Введите название картинки')
+    orig_img = Image.open('{}'.format(input()))
+    print('Выберите величину ячейки, на которую будут делить изображение\n' +
+          'Размер должен быть инициализирован целым положительным числом')
+    pic_size = int(input())
+    print('Выберите количество оттенков\n' +
+          'Количество должно быть инициализировано целым положительным числом')
+    grad = int(input())
+    arr = GreyImage(orig_img, pix_size=pic_size, gradation=grad).get_grey_image()
+    print('Введите название новой картинки')
+    name = input()
+    print('Введите формат новой картинки(jpg, png и т.д.)')
+    formt = input()
+    arr.save('{}.{}'.format(name, formt))
+    print('Если хотите завершить сессию, наберите \'yes\', если нет-нажмите enter')
+    if input() == 'yes':
+        flag = 'yes'

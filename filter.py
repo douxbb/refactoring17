0e6edfe1d5ee44a7d46d4a488706527cd2166802
Lilya <73344383+KingsWoman@users.noreply.github.com>
2021-11-14 18:00:20 +0300

    Step_1

--- filter.py
+++ filter.py
@@ -5,16 +5,16 @@ arr = np.array(img)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i < a - 11:
+while i < a:
     j = 0
-    while j < a1 - 11:
+    while j < a1:
         s = 0
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
+            for k in range(j, j + 10):
+                n1 = arr[n][k][0]
+                n2 = arr[n][k][1]
+                n3 = arr[n][k][2]
+                M = (int(n1) + int(n2) + int(n3)) / 3
                 s += M
         s = int(s // 100)
         for n in range(i, i + 10):

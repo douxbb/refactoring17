0689805f85e305db91d6c891630e38fb1e9a460d
Lingvistick <73380616+Lingvistick@users.noreply.github.com>
2021-11-14 19:37:14 +0500

    Шаг 4

--- filter.py
+++ filter.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import numpy as np
+
 class ConvertImage:
-    def __init__(self, image, size, step):
+    def __init__(self, image, size, gradation):
         self.image = image
         self.size = size
-        self.step = 255 // step
+        self.step = 255 // gradation
+
     def convert_image(self):
         height = len(self.image)
         width = len(self.image[1])
@@ -13,15 +15,11 @@ class ConvertImage:
                 medium_brightness = self.get_medium_brightness(i, j)
                 self.set_grayscale(medium_brightness, i, j)
         return Image.fromarray(self.image)
-
     def set_grayscale(self, medium_brightness, i, j):
         self.image[i:i + self.size, j:j + self.size] = int(medium_brightness // self.step) * self.step / 3
-
     def get_medium_brightness(self, i, j):
         return int((self.image[i:i + self.size, j:j + self.size].sum()) // self.size ** 2)
-
-
-original_image = Image.open("img2.jpg")
+    original_image = Image.open(input("Введите имя исходного изображения:"))
 pixels = np.array(original_image)
-result = ConvertImage(pixels, 10, 50).convert_image()
-result.save('res.jpg')
+result = ConvertImage(pixels, size=10, gradation=50).convert_image()
+result.save((input("Введите имя изображения, в которое запишется результат:")))

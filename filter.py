76f9777ec05e5460b9da576a21b69a0606b742b4
koyoot <koyoott@mail.ru>
2021-11-15 17:21:20 +0500

    Койнова Елизавета 1 этап

--- filter.py
+++ filter.py
@@ -5,17 +5,13 @@ arr = np.array(img)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i < a - 11:
+while i < a - (10 - 1):
     j = 0
-    while j < a1 - 11:
+    while j < a1 - (10 - 1):
         s = 0
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
+                s += (int(arr[n][n1][0]) + int(arr[n][n1][1]) + int(arr[n][n1][2])) // 3
         s = int(s // 100)
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):

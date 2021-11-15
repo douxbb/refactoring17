a6b1269e78036b0fd3ab181c00219b2d393f998b
koyoot <koyoott@mail.ru>
2021-11-15 20:44:13 +0500

    Койнова Елизавета 2 этап

--- filter.py
+++ filter.py
@@ -1,24 +1,29 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - (10 - 1):
-    j = 0
-    while j < a1 - (10 - 1):
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                s += (int(arr[n][n1][0]) + int(arr[n][n1][1]) + int(arr[n][n1][2])) // 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+def make_mosaic(array, grayscale:int, size_mosaic:int, row_length:int, column_length:int):
+    array = array[0: row_length // size_mosaic * size_mosaic, 0: column_length // size_mosaic * size_mosaic]
+    row_index = 0
+    while row_index < row_length - (size_mosaic - 1):
+        column_index = 0
+        while column_index < column_length - (size_mosaic - 1):
+            color = 0
+            for step_row in range(row_index, row_index + size_mosaic):
+                for step_column in range(column_index, column_index + size_mosaic):
+                    color += (int(array[step_row][step_column][0])
+                    + int(array[step_row][step_column][1])
+                    + int(array[step_row][step_column][2])) // 3
+            color = color // size_mosaic**2
+            for step_row in range(row_index, row_index + size_mosaic):
+                for step_column in range(column_index, column_index + size_mosaic):
+                    array[step_row][step_column][0] = (color // grayscale) * grayscale
+                    array[step_row][step_column][1] = (color // grayscale) * grayscale
+                    array[step_row][step_column][2] = (color // grayscale) * grayscale
+            column_index += size_mosaic
+        row_index += size_mosaic
+    return array
+
+img_array = np.array(Image.open("img2.jpg"))
+Image.fromarray(make_mosaic(img_array, int(input("Градации серого:  ")), 
+                                    int(input("Размер мозайки:  ")), len(img_array),
+                                    len(img_array[1]))).save('res.jpg')

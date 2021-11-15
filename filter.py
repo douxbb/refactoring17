bf76ce923bdf0632091de99e44c4f8395003747b
Ilya Krivonogov <ilkarik@yandex.ru>
2021-11-15 16:28:52 +0500

    Второй этап

--- filter.py
+++ filter.py
@@ -1,28 +1,33 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 1:
-    j = 0
-    while j < a1 - 1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                r = int(arr[n][n1][0])
-                g = int(arr[n][n1][1])
-                b = int(arr[n][n1][2])
-                M = (r+g+b)/3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+
+def mosaic_to_gray(array_img: np.array, mosiac_size, step_grad):
+    width = len(array_img)
+    height = len(array_img[1])
+    x = 0
+    while x < width:
+        y = 0
+        while y < height:
+            medium_color_mosaic = 0
+            for i in range(x, x + mosiac_size):
+                for j in range(y, y + mosiac_size):
+                    r = int(array_img[i][j][0])
+                    g = int(array_img[i][j][1])
+                    b = int(array_img[i][j][2])
+                    medium_color_mosaic += (r + g + b) / 3
+            medium_color_mosaic = int(medium_color_mosaic // (mosiac_size * mosiac_size))
+            for i in range(x, x + mosiac_size):
+                for j in range(y, y + mosiac_size):
+                    array_img[i][j][0] = int(medium_color_mosaic // step_grad) * step_grad
+                    array_img[i][j][1] = int(medium_color_mosaic // step_grad) * step_grad
+                    array_img[i][j][2] = int(medium_color_mosaic // step_grad) * step_grad
+            y = y + mosiac_size
+        x = x + mosiac_size
+    return array_img
+
+
+square_size = 10
+gradation = 5
+Image.fromarray(mosaic_to_gray(np.array(Image.open("img2.jpg")), square_size, gradation)).save('res.jpg')
+

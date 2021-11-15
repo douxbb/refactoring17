f73ed8f332591b262bf8154be386086913b35a42
Ilya Krivonogov <ilkarik@yandex.ru>
2021-11-15 19:12:07 +0500

    Третий этап

--- filter.py
+++ filter.py
@@ -5,25 +5,13 @@ import numpy as np
 def mosaic_to_gray(array_img: np.array, mosiac_size, step_grad):
     width = len(array_img)
     height = len(array_img[1])
-    x = 0
-    while x < width:
-        y = 0
-        while y < height:
-            medium_color_mosaic = 0
+    for x in range(0,width,mosiac_size):
+        for y in range(0,height, mosiac_size):
+            pixel_mosaic = array_img[x:x+mosiac_size,y:y+mosiac_size]
+            medium_color_mosaic = (np.sum(pixel_mosaic) / 3) // (mosiac_size * mosiac_size)
             for i in range(x, x + mosiac_size):
                 for j in range(y, y + mosiac_size):
-                    r = int(array_img[i][j][0])
-                    g = int(array_img[i][j][1])
-                    b = int(array_img[i][j][2])
-                    medium_color_mosaic += (r + g + b) / 3
-            medium_color_mosaic = int(medium_color_mosaic // (mosiac_size * mosiac_size))
-            for i in range(x, x + mosiac_size):
-                for j in range(y, y + mosiac_size):
-                    array_img[i][j][0] = int(medium_color_mosaic // step_grad) * step_grad
-                    array_img[i][j][1] = int(medium_color_mosaic // step_grad) * step_grad
-                    array_img[i][j][2] = int(medium_color_mosaic // step_grad) * step_grad
-            y = y + mosiac_size
-        x = x + mosiac_size
+                    array_img[i][j].fill((medium_color_mosaic // (step_grad*10))*(step_grad*10))
     return array_img
 
 

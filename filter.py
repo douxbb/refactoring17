29301654cbe7c22d1336ce7dc0ab42f3a418cefe
sofity <stssofia@mail.ru>
2021-11-29 10:08:56 +0500

    Step 3

--- filter.py
+++ filter.py
@@ -1,22 +1,14 @@
 from PIL import Image
 import numpy as np
 def get_pixel(i, j):
-    sum = 0
-    for n in range(i, i + pxSize):
-        for m in range(j, j + pxSize):
-            r = arr[n][m][0]
-            g = arr[n][m][1]
-            b = arr[n][m][2]
-            sum += (int(r) + int(g) + int(b)) / 3
-    return int(sum // (pxSize*pxSize))
+    sum = np.sum(arr[i:i + pxSize, j: j + pxSize]) / 3
+    return int(sum // (pxSize * pxSize))
 
 
 def set_grey_pixels(colorSum, i, j):
-    for n in range(i, i + pxSize):
-        for m in range(j, j + pxSize):
-            arr[n][m][0] = int(colorSum // gradation) * gradation
-            arr[n][m][1] = int(colorSum // gradation) * gradation
-            arr[n][m][2] = int(colorSum // gradation) * gradation
+    arr[i:i + pxSize, j:j + pxSize, 0] = int(colorSum // gradation) * gradation
+    arr[i:i + pxSize, j:j + pxSize, 1] = int(colorSum // gradation) * gradation
+    arr[i:i + pxSize, j:j + pxSize, 2] = int(colorSum // gradation) * gradation
 img = Image.open("img2.jpg")
 arr = np.array(img)
 height = len(arr)
